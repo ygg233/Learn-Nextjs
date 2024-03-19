4553cdd038fcafbde491cfb05e28e58a50613b91
Yang Gao (FESCO Adecco Human Resources) <v-gaoyang@microsoft.com>
2024-03-19 21:26:47 +0800

    Chapter 6.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Anek_Tamil } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight:"400", subsets: ['latin']});
+
+export const anekTamil = Anek_Tamil({weight: "300", subsets: ['latin']});
